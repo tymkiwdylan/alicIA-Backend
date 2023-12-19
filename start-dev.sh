d1249a61db3d4ec70f5d20843bc77cb9b0569ccb
Dylan Tymkiw <tymkiwdylan@gmail.com>
2023-12-19 16:35:29 +0000

    from server

--- start-dev.sh
+++ start-dev.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Pull code 
-git pull -f
-
-source myvenv/bin/activate
+git pull
 
 # Start API 1
-nohup python stock_service/data_layer/app.py
+nohup python stock_service/data_layer/app.py &
 
-nohup python stock_service/chain_layer/main.py
+nohup python stock_service/chain_layer/main.py &
 
 # Start API 3
-nohup python3 customer_service/main.py
+nohup python3 customer_service/main.py &
 
 # Start ngrok
-nohup ngrok start --all
+nohup ngrok start --all &
 
 echo "All APIs and Ngrok started."
