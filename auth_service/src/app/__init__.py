a689303805e2d585387b980f6a1fb482ca5fab41
tymkiwdylan <datymk23@colby.edu>
2024-02-28 12:00:15 -0300

    commit

--- auth_service/src/app/__init__.py
+++ auth_service/src/app/__init__.py
@@ -2,25 +2,27 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate  # Import Flask-Migrate
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI') # Use appropriate database
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')  # Use appropriate database
     app.config['SECRET_KEY'] = 'your-secret-key'  # Change to a random secret
-    # CORS(app)
     
-    from .models import User
     db.init_app(app)
-    create_db(app)
+    migrate = Migrate(app, db)  # Initialize Flask-Migrate
+
     CORS(app)
+    
+    # Import models here to ensure they are known to Flask-Migrate before running migrations
+    from .models import User
+
+    # The create_db function is not needed anymore since Flask-Migrate handles database initialization
+    # from .models import User
+
     from .routes import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     return app
-
-def create_db(app):
-    if not(os.path.exists('instance/auth.db')):
-        with app.app_context():
-            db.create_all()
