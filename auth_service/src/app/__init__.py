420f6ddae64a5fb08a863964d931207727b73d31
tymkiwdylan <datymk23@colby.edu>
2024-02-28 11:30:27 -0300

    cloud proxy

--- auth_service/src/app/__init__.py
+++ auth_service/src/app/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'  # Use appropriate database
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI') # Use appropriate database
     app.config['SECRET_KEY'] = 'your-secret-key'  # Change to a random secret
     # CORS(app)
     
