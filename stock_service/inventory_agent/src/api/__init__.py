51194a0e5c803124049d42428aadc10f8263158b
tymkiwdylan <datymk23@colby.edu>
2024-03-07 11:15:43 -0300

    hotfix

--- stock_service/inventory_agent/src/api/__init__.py
+++ stock_service/inventory_agent/src/api/__init__.py
@@ -43,7 +43,7 @@ functions = load_tools_from_directory('functions')
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://alicia:alicia@34.95.254.138/customer" 
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://alicia:alicia@34.95.254.138/inventory" 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'secret'
     CORS(app)
